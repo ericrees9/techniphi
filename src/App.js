9dd44b846d646d7c96f4af8fdc0a524772b59141
erees <erees@protonmail.com>
2020-01-20 12:13:14 -0500

    Building initial state into form.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Form, Field } from 'react-final-form'
 import MovieCard from './components/MovieCard/MovieCard';
 
-const onSubmit = (values) => {
-  console.log(`${JSON.stringify(values, 0, 2)}`);
-}
-
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const onSubmit = (values) => {
+    // console.log(`${JSON.stringify(values)}`);
+    movies.push(values);
+    console.log(movies);
+  }
+  
   return (
     <div className="App">
       <div className="formDiv">
         <Form 
           onSubmit={onSubmit}
-          render={({ handleSubmit, values }) => (
+          render={({ handleSubmit, form, values }) => (
             <form onSubmit={handleSubmit}>
             <h2>Test Form</h2>
             <div>
               <label>Movie Title: </label>
-              <Field name="Movie Title" component="input" placeholder="Movie Title" />
+              <Field name="movieTitle" component="input" placeholder="Movie Title" />
+            </div>
+            <div>
+              <label>Release Year: </label>
+              <Field name="movieYear" component="input" placeholder="2020" />
             </div>
             <div>
               <label>Movie Rating: </label>
@@ -30,18 +38,22 @@ function App() {
                 <option>R</option>
               </Field>
             </div>
-            <div>
-              <label>Release Year: </label>
-              <Field name="Movie Year" component="input" placeholder="2020" />
-            </div>
 
-            <button type="submit">Submit</button>
+            <button 
+              type="submit"
+            >
+              Submit
+            </button>
             </form>
           )}
         />
       </div>
       <div className="dataDiv">
-        <MovieCard />
+        {movies.map((movie, index) => {
+          return(
+            <MovieCard movieData={movie}/>
+          )
+        })}
       </div>
     </div>
   );
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../MovieCard/MovieCard.css';
 
-function MovieCard () {
+function MovieCard (props) {
+    console.log(props);
     return(
         <div className="cardBox">
-            <h2>This will be the movie card.</h2>
+            <h2>Test</h2>
         </div>
     );
 }
