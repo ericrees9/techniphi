ad6ae8d344df2d9c7074d9be84031c26125fe6a0
erees <erees@protonmail.com>
2020-01-20 21:47:54 -0500

    Added dialog to MovieCard.

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import '../MovieCard/MovieCard.css';
-import { Typography, Card, Container } from '@material-ui/core';
+import { Typography, Card, Container, Dialog, DialogTitle, DialogContent, Button, DialogActions } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const MovieCard = (props) => {
     const [favorite, setFavorite] = useState(false);
-    // console.log(props)
+    const [open, setOpen] = useState(false);
+    
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return(
         <Card className="cardBox" variant="outlined">
             <Container className="dataArea">
@@ -19,9 +28,26 @@ const MovieCard = (props) => {
                 <IconButton onClick={() => setFavorite(!favorite)}>
                     <FavoriteIcon color={favorite == true ? "secondary" : "disabled"}/>
                 </IconButton>
-                <IconButton onClick={() => props.deleteMovie(props.movieData.movieTitle)}>
+                <IconButton onClick={handleOpen}>
                     <DeleteIcon />
                 </IconButton>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogTitle id="alert-dialog-title">{"Delete This Movie?"}</DialogTitle>
+                    <DialogContent>
+                        Are you sure you want to delete this movie?
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="disabled">
+                            No
+                        </Button>
+                        <Button onClick={handleClose} onClick={() => props.deleteMovie(props.movieData.movieTitle)} color="secondary" autoFocus>
+                            Yes
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Container>
         </Card>
     );
